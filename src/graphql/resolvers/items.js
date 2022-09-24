54e6e8f43728fc8107d65baa6e42b28985c61f67
Moaz A.Moshtha <moazmoshtha@gmail.com>
2022-09-24 21:48:36 +0300

    connect to db

--- src/graphql/resolvers/items.js
+++ src/graphql/resolvers/items.js
@@ -1,19 +1,43 @@
-const items = [];
+const Item = require('../../models/item');
 
 module.exports = {
+  //get all items
   items: async () => {
-    return [...items];
+    try {
+      const items = await Item.find();
+      return items.map(item => {
+        return transformItem(item);
+      });
+    } catch (err) {
+      throw err;
+    }
   },
+
+  //create item
   createItem: async args => {
-    const { name, description, status } = args.itemInput;
-    const item = {
-      _id: Math.random().toString(),
-      name,
-      description,
-      status,
-    };
-    console.log(args);
-    items.push(item);
-    return item;
-  }
+    const item = new Item({
+      name: args.itemInput.name,
+      description: args.itemInput.description,
+      status: args.itemInput.status
+    });
+    try {
+      //save function returns a promise created by mongoose
+      const result = await item.save();
+      return transformItem(result);
+    } catch (err) {
+      throw err;
+    }
+  },
 };
+
+/**
+ * @description
+ * return item (core properties only, and leave metadata out) and transform the _id to a string
+ * _id is an object, and we want to return a string
+ */
+const transformItem = item => {
+  return {
+    ...item._doc,
+    _id: item.id,
+  };
+}
